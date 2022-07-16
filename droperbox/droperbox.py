841fa41d71427f34d0c2f3be83e68ec40535b50e
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2022-07-16 17:27:15 +0900

    Update droperbox.py

--- droperbox/droperbox.py
+++ droperbox/droperbox.py
@@ -1,11 +1,40 @@
 from dropbox import Dropbox
 
-def download(token,file_name):
-    dbx = dropbox.Dropbox(token)
-    dbx.users_get_current_account()
-    with open(file_name, "wb") as f:
-        metadata, res = dbx.files_download(path=f"/{file_name}")
-        f.write(res.content)
-    
-def upload()
+_token = {
+    "token": None
+}
 
+def token(token):
+    _token["token"] = token
+
+def get_token():
+    return _token["token"]
+
+def create_dbx():
+    token = get_token()
+    if token != None:
+        dbx = dropbox.Dropbox()
+        dbx.users_get_current_account()
+        return dbx
+
+def download(token,file_name):
+    try:
+        dbx = create_dbx()
+        with open(file_name, "wb") as f:
+            metadata, res = dbx.files_download(path=f"/{file_name}")
+            f.write(res.content)
+            return True
+    except Exception as e:
+        print(e)
+        return False
+    
+    
+def upload(file_name)
+    try:
+        dbx = create_dbx()
+        with open(file_name, "rb") as fc:
+            dbx.files_upload(fc.read(), f"/{file_name}", mode=dropbox.files.WriteMode.overwrite)
+        return True
+    except Exception as e:
+        print(e)
+        return False

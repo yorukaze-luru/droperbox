38750a837f75f4390b319dc89b3f9abaa8db3ce8
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2022-07-16 16:00:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+install_requires = [
+    'dropbox'
+]
+
 setup(
     name='droperbox',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=install_requires
 )

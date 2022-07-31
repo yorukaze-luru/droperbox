ef4c43b066b4fb23eb67e39bcfaf57409aa0304c
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2022-07-31 23:48:54 +0900

    Update droperbox.py

--- droperbox/droperbox.py
+++ droperbox/droperbox.py
@@ -1,5 +1,3 @@
-import dropbox
-
 _token = {
     "token": None
 }
@@ -10,16 +8,19 @@ def token(token):
 def get_token():
     return _token["token"]
 
-def create_dbx():
+def create_dbx(dropbox):
     token = get_token()
     if token != None:
         dbx = dropbox.Dropbox()
         dbx.users_get_current_account()
         return dbx
+    else:
+        print('Not Token')
+        return False
 
-def download(file_name:int):
+def download(dropbox,file_name:int):
     try:
-        dbx = create_dbx()
+        dbx = create_dbx(dropbox)
         with open(file_name, "wb") as f:
             metadata, res = dbx.files_download(path=f"/{file_name}")
             f.write(res.content)
@@ -29,9 +30,9 @@ def download(file_name:int):
         return False
     
     
-def upload(file_name:int):
+def upload(dropbox,file_name:int):
     try:
-        dbx = create_dbx()
+        dbx = create_dbx(dropbox)
         with open(file_name, "rb") as fc:
             dbx.files_upload(fc.read(), f"/{file_name}", mode=dropbox.files.WriteMode.overwrite)
         return True

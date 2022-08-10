a7065257c48f28106f654eb2b2ada073f0c1e10a
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2022-08-10 10:38:50 +0900

    Update droperbox.py

--- droperbox/droperbox.py
+++ droperbox/droperbox.py
@@ -19,17 +19,23 @@ def create_dbx(dropbox):
     
 def download(dropbox,file_name:int):
     try:
-        dbx = create_dbx(dropbox)
-        dbx.files_download_to_file(f'{file_name}', f'/{file_name}')
-        return True, 'Success'
+        check, dbx = create_dbx(dropbox)
+        if check == False:
+            return False, dbx
+        elif check == True:
+            dbx.files_download_to_file(f'{file_name}', f'/{file_name}')
+            return True, 'Success'
     except Exception as e:
         return False, e
     
     
 def upload(dropbox,file_name:int):
     try:
-        dbx = create_dbx(dropbox)
-        dbx.files_upload(open(f'{file_name}', 'rb').read(), f'/{file_name}')
-        return True, 'Success'
+        check, dbx = create_dbx(dropbox)
+        if check == False:
+            return False, dbx
+        elif check == True:
+            dbx.files_upload(open(f'{file_name}', 'rb').read(), f'/{file_name}')
+            return True, 'Success'
     except Exception as e:
         return False, e

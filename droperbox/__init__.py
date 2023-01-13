fc2a57f602f5674282b3fdf635e7291d38df1ff3
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2023-01-13 16:39:44 +0900

    Update __init__.py

--- droperbox/__init__.py
+++ droperbox/__init__.py
@@ -2,11 +2,4 @@ from droperbox.droperbox import Droperbox
 
 __version__ = '2.0.1'
 
-from os.path import dirname, abspath
-import sys
-
-parent_dir = dirname(abspath(__file__)) # 追加
-if parent_dir not in sys.path: # 追加
-    sys.path.append(parent_dir) # 追加
-
 from dropbox import Dropbox
